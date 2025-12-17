6539d9239253475c3cf2852223c4b1b6463a224f
hydralgorithm <hydralgorithm@gmail.com>
2025-12-18 00:59:41 +0530

    adjusted meteor height

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -76,7 +76,7 @@ export const StarBackground =  () => {
             {meteors.map((meteor) => (
                 <div key={meteor.id} className="meteor animate-meteor" style={{
                     width: meteor.size * 50 + "px",
-                    height: meteor.size * 2 + "px",
+                    height: meteor.size + "px",
                     left: meteor.x + "%",
                     top: meteor.y + "%",
                     animationDelay: meteor.delay,
